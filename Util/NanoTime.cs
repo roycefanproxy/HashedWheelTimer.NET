30f6c4a17138206981c89870268d4c18e9c64c0d
Royce Fan <fchunhuei@gmail.com>
2018-05-14 10:09:59 +0800

    change file structure.

--- Util/NanoTime.cs
+++ Util/NanoTime.cs
@@ -6,7 +6,6 @@ namespace ricefan123.Timer.Util
     public static class NanoTime 
     {
 
-
         public static long FromMilliseconds(long milli) => milli * 1000000;
         public static long FromSeconds(long sec) => sec * 1000000000;
 
