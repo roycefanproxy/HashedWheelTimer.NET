11992fc133e4b3a458b9de919ef166ce18c52d44
Royce Fan <fchunhuei@gmail.com>
2018-05-14 10:04:14 +0800

    add new file.

---
+++ src/Util/NanoTime.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+
+namespace ricefan123.Timer.Util
+{
+    public static class NanoTime 
+    {
+
+
+        public static long FromMilliseconds(long milli) => milli * 1000000;
+        public static long FromSeconds(long sec) => sec * 1000000000;
+
+        public static int ToMilliseconds(long nano) => (int) nano / 1000000;
+    }
+
+    public class HighResolutionTimer
+    {
+        private object l = new object();
+        private Stopwatch watch = new Stopwatch();
+
+        public void Start()
+        {
+            watch.Start();
+        }
+        public long Elapsed
+        {
+            get
+            {
+                double elapsed;
+                lock (l)
+                {
+                    elapsed = watch.ElapsedTicks;
+                }
+                return (long) elapsed * Stopwatch.Frequency / 1000000000;
+            }
+        }
+    }
+}
